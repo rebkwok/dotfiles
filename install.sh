6555edb55eca231c0fb180a53706d1f63d7dc447
Mark Smith <mark.smith@practicalpoetry.co.uk>
2014-11-19 14:08:20 +0000

    Finally symlinks all binaries into ~/bin

--- install.sh
+++ install.sh
@@ -14,3 +14,21 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 if [[ ! -e ~/.vim ]]; then
 	ln -s ~/.dotfiles/vim ~/.vim
 fi
+
+#-----------------------------------------------------------------------------
+# Link all binaries into ~/bin
+#-----------------------------------------------------------------------------
+
+dotfiles_home=${HOME}/.dotfiles
+
+# If using the old symlink ~/bin -> ~/.dotfiles/bin, delete and replace:
+if [ -L ~/bin ] && [ $(readlink ~/bin) == "${dotfiles_home}/bin" ]; then
+    echo 'Removing ~/bin symlink'
+    rm ~/bin &&  mkdir ~/bin
+fi
+
+for f in $(ls ${dotfiles_home}/bin); do
+    ln -s "${dotfiles_home}/bin/$f" "${HOME}/bin"
+done
+
+
